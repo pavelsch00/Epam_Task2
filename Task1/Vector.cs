c8fd2c931ceacdac94fbac1c03b1dcb014c6148c
Павел Шишковец <pavelsch00@gmail.com>
2020-07-14 20:28:22 +0300

    Добавьте файлы проекта.

---
+++ Task1/Vector.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task1
+{
+    public class Vector
+    {
+        public Vector(int x, int y, int z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        public int Z { get; set; }
+
+        public static Vector operator + (Vector vector1, Vector vector2) => new Vector(vector1.X + vector2.X, vector1.Y + vector2.Y, vector1.Z + vector2.Z);
+        public static Vector operator - (Vector vector1, Vector vector2) => new Vector(vector1.X - vector2.X, vector1.Y - vector2.Y, vector1.Z - vector2.Z);
+
+    }
+}
