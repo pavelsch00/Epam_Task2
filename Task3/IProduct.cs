00c8727e00bfa9291241b1f0ef98bf1adcf9ba1a
Павел Шишковец <pavelsch00@gmail.com>
2020-07-20 00:12:59 +0300

    Task 2-3: Product class and interface created

---
+++ Task3/IProduct.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task3
+{
+    interface IProduct
+    {
+        /// <summary>
+        /// The field describes the type of product.
+        /// </summary>
+        public string Type { get; set; }
+
+        /// <summary>
+        /// The field describes the name of the product.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The field describes the price of the product.
+        /// </summary>
+        public  decimal Price { get; set; }
+    }
+}
